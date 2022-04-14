b6acef9f2fb5c6d2e90bc368d63b1ca9b23b05bd
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-14 18:59:08 +0300

    add main class

--- src/main/App.java
+++ src/main/App.java
@@ -1,4 +1,26 @@
 package main;
 
+import java.util.Scanner;
+
 public class App {
-}
+
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String s = in.nextLine();
+
+        //to find the position of first vowel
+        int i;
+        for(i=0; i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+                break; //means vowel is found at i
+        }
+
+        String f = s.substring(i) + s.substring(0,i) + "ay";
+
+        System.out.println(f);
+    }
+    }
